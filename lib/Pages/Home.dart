c63db1e9265bb3b11e9c635a5ff3b2be7c468b2d
George Bjork <georgebjork@outlook.com>
2022-01-17 21:19:26 -0800

    added nav buttons

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:golf_app/Utils/Providers/UserProvider.dart';
 import 'package:golf_app/Utils/all.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
+import '../Components/NavWidget.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,7 +11,6 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -20,16 +18,30 @@ class HomeState extends State<Home> {
   }
 
   Widget build(BuildContext context) {
+    final themeProvider = context.read<ThemeProvider>();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         leading: const Icon(Icons.menu_rounded),
       ),
       body: Container(
-        child: Column(  
+        child: Column(
           children: [
             PlayerDetails(),
-            
+            Container(
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0), //add left right padding
+                child: NavWidget(
+                  btn1text: 'Stats',
+                  btn1Color: themeProvider.getTan(),
+                  btn1onPressed: () {},
+                  btn2text: 'Friends',
+                  btn2Color: themeProvider.getTan(),
+                  btn2onPressed: () {},
+                  btn3text: 'Start Round',
+                  btn3Color: themeProvider.getOrange(),
+                  btn3onPressed: () {},
+                ))
           ],
         ),
       ),
@@ -37,48 +49,59 @@ class HomeState extends State<Home> {
   }
 }
 
-
 class PlayerDetails extends StatefulWidget {
   @override
   PlayerDetailsState createState() => PlayerDetailsState();
 }
 
 class PlayerDetailsState extends State<PlayerDetails> {
-  
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 20.0, right: 20.0), //add left right padding
-      width: double.infinity, //width takes up whole screen
-      height: MediaQuery.of(context).size.height * 0.3333, //height only takes up part of the screen
-      decoration: BoxDecoration(
-          color: Theme.of(context).primaryColor,
-          //Add rounded border to the bottom of the container
-          borderRadius: const BorderRadius.only(
-              bottomRight: Radius.circular(35),
-              bottomLeft: Radius.circular(35))),
-      child: Consumer<UserProvider> (builder: (context, provider, child) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Welcome,', style: Theme.of(context).textTheme.headline2,),
-            Text(provider.firstName, style: Theme.of(context).textTheme.headline2,),
-
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Handicap', style: Theme.of(context).textTheme.headline3,),
-                    Text(provider.getHandicap().toString(), style: Theme.of(context).textTheme.headline1),
-                    Text('Low:', style: Theme.of(context).textTheme.headline3,)
-                  ],
-                ),
+        padding: const EdgeInsets.only(
+            left: 20.0, right: 20.0), //add left right padding
+        width: double.infinity, //width takes up whole screen
+        height: MediaQuery.of(context).size.height *
+            0.3333, //height only takes up part of the screen
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            //Add rounded border to the bottom of the container
+            borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(35),
+                bottomLeft: Radius.circular(35))),
+        child: Consumer<UserProvider>(builder: (context, provider, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome,',
+                style: Theme.of(context).textTheme.headline2,
               ),
-            )
-          ],
-        );
-      }
-    ));
+              Text(
+                provider.firstName,
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Handicap',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      Text(provider.getHandicap().toString(),
+                          style: Theme.of(context).textTheme.headline1),
+                      Text(
+                        'Low:',
+                        style: Theme.of(context).textTheme.headline3,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        }));
   }
 }
