0f71c0478ca32d92ff629e568cc3f13b30ac0554
George Bjork <georgebjork@outlook.com>
2022-01-18 21:25:06 -0800

    added data for teebox

--- lib/Utils/TeeBox.dart
+++ lib/Utils/TeeBox.dart
@@ -1,3 +1,11 @@
 class TeeBox {
-  
+
+    int id;
+    int courseId;
+    String name;
+    int slope;
+    double rating;
+    int teeBoxType;
+
+    TeeBox(this.id, this.courseId, this.name, this.slope, this.rating, this.teeBoxType);
 }
