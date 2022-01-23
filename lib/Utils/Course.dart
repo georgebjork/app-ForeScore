49eea8111c2da16144a6080993c59287b95d9343
George Bjork <georgebjork@outlook.com>
2022-01-22 17:56:17 -0800

    now pulls in teebox info on course api requests

--- lib/Utils/Course.dart
+++ lib/Utils/Course.dart
@@ -1,19 +1,28 @@
 
+import 'TeeBox.dart';
+
 class Course {
+
   int id;
   String name;
+  //String state;
+  List<TeeBox> teeboxes = [];
 
   Course(this.id, this.name);
 
   Course.fromCourse({
     required this.id,
-    required this.name
+    required this.name,
+    required this.teeboxes
   });
 
   factory Course.fromJson(dynamic res){
+    List<dynamic> teeBox = res['teeBoxes'];
     return Course.fromCourse(
       id: res['id'], 
-      name: res['name']
+      name: res['name'],
+      teeboxes: teeBox.map((e) => TeeBox.fromJson(e)).toList()
+
     );
   }
 }
--- lib/Utils/TeeBox.dart
+++ lib/Utils/TeeBox.dart
@@ -4,8 +4,34 @@ class TeeBox {
     int courseId;
     String name;
     int slope;
+    int par;
+    int adjustment;
     double rating;
     int teeBoxType;
 
-    TeeBox(this.id, this.courseId, this.name, this.slope, this.rating, this.teeBoxType);
+    TeeBox(this.id, this.courseId, this.name, this.slope, this.rating, this.teeBoxType, this.par, this.adjustment);
+
+    TeeBox.fromTeeBox({
+      required this.id, 
+      required this.courseId, 
+      required this.name, 
+      required this.slope, 
+      required this.rating,
+      required this.teeBoxType,
+      required this.par,
+      required this.adjustment
+    });
+
+    factory TeeBox.fromJson(dynamic res){
+      return TeeBox.fromTeeBox(
+        id: res['id'],
+        courseId: res['courseId'],
+        name: res['name'],
+        slope: res['slope'],
+        par: res['par'],
+        rating: res['rating'],
+        teeBoxType: res['type'],
+        adjustment: res['adjustment']
+      );
+    }
 }
