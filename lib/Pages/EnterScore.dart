d2e16920d2a65494bc3b7ccf6aea47a74e494bea
George Bjork <georgebjork@outlook.com>
2022-01-26 21:11:18 -0800

    Start of an enter score page

--- lib/Pages/EnterScore.dart
+++ lib/Pages/EnterScore.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:golf_app/Utils/Providers/MatchProvider.dart';
+import 'package:provider/provider.dart';
+
+import '../Components/NavWidget.dart';
 
 class EnterScore extends StatefulWidget {
 
@@ -6,7 +10,43 @@ class EnterScore extends StatefulWidget {
 }
 
 class EnterScoreState extends State<EnterScore> {
+
   Widget build(context){
-    return Container(child: Center(child: Text("Hello world"),),);
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        leading: null,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: Consumer<MatchProvider>(
+        builder: (context, provider, child) {
+          return Container(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(provider.match.course.name, style: Theme.of(context).primaryTextTheme.headline3),
+                const SizedBox(height: 5,),
+                Text(provider.displayCurrentHole(), style: Theme.of(context).primaryTextTheme.headline2),
+                Text(provider.displayHoleDetails(), style: Theme.of(context).primaryTextTheme.headline4),
+                const SizedBox(height: 10,),
+        
+                Expanded(child: Center(child: Text('Hello'),)),
+        
+                NavWidget(
+                  btn1text: 'Prev',
+                  btn1onPressed: () => provider.prevHole(),
+                  btn2text: 'Next',
+                  btn2onPressed: () => provider.nextHole(),
+                  btn3text: 'Cancel',
+                  btn3onPressed: () {}
+                ),
+                
+                const SizedBox(height: 30)
+              ]
+            ),
+          );
+        }),
+    );
   }
 }
--- lib/Utils/Providers/MatchProvider.dart
+++ lib/Utils/Providers/MatchProvider.dart
@@ -5,8 +5,35 @@ import '../Match.dart';
 class MatchProvider extends ChangeNotifier{
 
   late Match match;
+  int currentHole = 0;
 
   MatchProvider();
+
+  void nextHole(){
+    if(currentHole < 17){
+      currentHole++;
+      notifyListeners();
+    }
+  }
+
+  void prevHole(){
+    if(currentHole > 0){
+      currentHole --;
+      notifyListeners();
+    }
+  }
+
+  String displayCurrentHole(){
+    int hole = currentHole+1;
+    return "Hole " + hole.toString();
+  }
+
+  String displayHoleDetails(){
+    String par = match.teeBox.holes[currentHole].number.toString();
+    String hdcp = match.teeBox.holes[currentHole].handicap.toString();
+
+    return "Par: " + par + " | " + "Hdcp: " + hdcp;
+  }
 
 
   void setMatch(Match m){
